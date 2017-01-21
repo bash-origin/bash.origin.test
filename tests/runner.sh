700356b4dd01bab0dfd769120a1e687532688cfb
Christoph Dorn <christoph@christophdorn.com>
2017-01-20 20:17:32 -0800

    wip

--- tests/runner.sh
+++ tests/runner.sh
@@ -5,7 +5,8 @@ cmd="$1"
 shift
 [ -z "$BO_VERBOSE" ] || BO_log "$BO_VERBOSE" "[bash.origin.test][runner.sh] Calling: $cmd $@"
 set +e
-"$cmd" "$@"
+export SHELL_RESOLVED=$(which bash)
+"$SHELL_RESOLVED" "$cmd" "$@"
 rc=$?
 if [[ $rc != 0 ]]; then
     echo "[exit code: $rc]"
