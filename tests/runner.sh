7da1ad7205f18bf0854bfe43505b2b6474c7a683
Christoph Dorn <christoph@christophdorn.com>
2017-01-20 19:19:02 -0800

    wip

--- tests/runner.sh
+++ tests/runner.sh
@@ -3,10 +3,12 @@
 BO_resetLoaded
 cmd="$1"
 shift
+[ -z "$BO_VERBOSE" ] || BO_log "$BO_VERBOSE" "[bash.origin.test][runner.sh] Calling: $cmd $@"
+set +e
 "$cmd" "$@"
 rc=$?
 if [[ $rc != 0 ]]; then
-    echo "exit code: $rc"
+    echo "[exit code: $rc]"
     exit 1
 fi
 exit 0
